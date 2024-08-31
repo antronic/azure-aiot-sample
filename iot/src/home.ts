af5a33cacb35458b0ae417005974264142ecbc5b
Jirachai C <jirachai.c@outlook.com>
2024-08-31 12:51:53 +0700

    Find by dev id

--- iot/src/home.ts
+++ iot/src/home.ts
@@ -4,11 +4,11 @@ import { DeviceOptions } from './utils/device'
 
 async function main() {
   const livimgRoomClimateConfig: DeviceOptions = {
-    deviceId: 'dev-h-02',
+    deviceId: 'dev-02',
     deviceName: 'Living Room Climate',
     modelId: 'dtmi:aiot:group1:Climate;1',
     location: 'LIVING_ROOM',
-    key: '46AFa0tODQfBrPmxE8K8GZ9F2WNKY8NsxAIoTAVg0+g=',
+    key: 'yJ2G9PBRnyfTvWJCZtqIziv/KRP34oCpuAIoTFdeFps=',
     energyConsumption: 3500
   }
   const livingRoomClimate = new Climate(livimgRoomClimateConfig)
